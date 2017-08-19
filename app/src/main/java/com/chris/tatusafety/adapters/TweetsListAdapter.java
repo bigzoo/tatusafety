4d8d7b2782f89044a92034da012646f054f12d29
yonkokilasi <yonkokilasi@gmail.com>
2017-08-19 23:58:54 +0300

    figured out time stamp . tweets now show e.g one hour ago

--- app/src/main/java/com/chris/tatusafety/adapters/TweetsListAdapter.java
+++ app/src/main/java/com/chris/tatusafety/adapters/TweetsListAdapter.java
@@ -26,7 +26,11 @@ import com.chris.tatusafety.UI.TweetsActivity;
 
 import org.parceler.Parcels;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -40,6 +44,10 @@ public class TweetsListAdapter extends RecyclerView.Adapter<TweetsListAdapter.Tw
     private ArrayList<Tweet> mTweets = new ArrayList<>();
     private Context mContext;
     private Tweet mTweet;
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
 
     public TweetsListAdapter(Context context,ArrayList<Tweet> tweets){
         mContext = context;
@@ -81,14 +89,39 @@ public class TweetsListAdapter extends RecyclerView.Adapter<TweetsListAdapter.Tw
             mContext = itemView.getContext();
             itemView.setOnClickListener(this);
 
-
-
         }
+
         private void bindTweet(Tweet tweet) {
             mTweetUser.setText(tweet.getUser());
             mUserTextView.setText("@"+ tweet.getUser());
             mTweetTextView.setText(tweet.getTweetText());
-            mDates.setText(tweet.getDate());
+            String givenDateString = tweet.getDate();
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+            try {
+                Date mDate = sdf.parse(givenDateString);
+                long timeInMilliseconds = mDate.getTime();
+                long now = System.currentTimeMillis();
+                final long diff = now - timeInMilliseconds;
+                if (diff < MINUTE_MILLIS) {
+                    mDates.setText("just now");
+                } else if (diff < 2 * MINUTE_MILLIS) {
+                    mDates.setText("a minute ago");
+                } else if (diff < 50 * MINUTE_MILLIS) {
+                    mDates.setText(diff / MINUTE_MILLIS + " minutes ago");
+                } else if (diff < 90 * MINUTE_MILLIS) {
+                    mDates.setText("an hour ago");
+                } else if (diff < 24 * HOUR_MILLIS) {
+                    mDates.setText(diff / HOUR_MILLIS + " hours ago");
+                } else if (diff < 48 * HOUR_MILLIS) {
+                    mDates.setText("yesterday");
+                } else {
+                    mDates.setText(diff / DAY_MILLIS + " days ago");
+                }
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
         }
 
         @Override
@@ -109,4 +142,5 @@ public class TweetsListAdapter extends RecyclerView.Adapter<TweetsListAdapter.Tw
                 mContext.startActivity(intent);
         }
     }
+
 }
